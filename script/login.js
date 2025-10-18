e9c82ce7b10ceb98ef0a5b8f8b74f5f7192f3aa4
AHM MRK <ahmmrk.3570m@gmail.com>
2025-10-18 21:27:06 +0330

    Update login.js

--- script/login.js
+++ script/login.js
@@ -19,7 +19,7 @@ function searchPerson() {
           password.value = '';
           localStorage.setItem('personDetail', JSON.stringify(personalData));
           setTimeout(() => {
-            window.location.href = "dashboard.html";
+            location.href = "dashboard.html";
           },2000);
         } else {
           document.querySelector('.password').style.border = '1px solid red';
@@ -35,6 +35,7 @@ document.querySelector('#js-login-btn').addEventListener('click', () => {
   searchPerson();
 
 });
+
 
 
 
